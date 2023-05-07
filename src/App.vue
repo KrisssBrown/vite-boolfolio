a7c02c4875d781a0c9ed314a5ac80722f76a3e7d
KrisssBrown <cristianbruno.cs@gmail.com>
2023-05-07 14:52:04 +0200

    FEAT add axios call

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <Projects />
+  </div>
+</template>
+
+<script>
+import Projects from './components/projects.vue';
+  export default {
+    components:{
+      Projects
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/Projects.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div class="container">
+      <h6 v-for="project in projects" :key="projects.id">
+        {{ project.nome_progetto }}
+      </h6>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import axios from 'axios'
+export default {
+  data() {
+    return {
+      projects: []
+    }
+  },
+  methods: {
+    fetchProjects() {
+      axios.get('http://127.0.0.1:8000/api/projects').then(res => {
+        const { results } = res.data
+        this.projects = results
+      }).catch(err =>{
+        console.log(err)
+      })
+    }
+  },
+  mounted(){
+    this.fetchProjects()
+  }
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+
+</style>
